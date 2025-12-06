c29cfe47cff2bbc3504fce2b02a867710f226287
Manulv03 <manu.estudios03@gmail.com>
2025-12-05 20:00:50 -0500

    initial commit with Java Project

---
+++ auth_catalog_kata/src/main/java/org/manudev/auth_catalog_kata/AuthCatalogKataApplication.java
@@ -0,0 +1,13 @@
+package org.manudev.auth_catalog_kata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthCatalogKataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthCatalogKataApplication.class, args);
+    }
+
+}
---
+++ auth_catalog_kata/src/test/java/org/manudev/auth_catalog_kata/AuthCatalogKataApplicationTests.java
@@ -0,0 +1,13 @@
+package org.manudev.auth_catalog_kata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthCatalogKataApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
